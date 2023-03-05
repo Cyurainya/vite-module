26c24789b70b8364a7c9af1e4919f5572c180c0e
yannis Cyu <yanniscyu@yannisdeMacBook-Air.local>
2023-03-05 19:07:03 +0800

    feat: resolve is not a function問題需要解決

--- plugins/virtual-module.ts
+++ plugins/virtual-module.ts
@@ -1,18 +1,34 @@
-export default function myPlugin() {
-  const virtualModuleId = 'virtual:my-module'
-  const resolvedVirtualModuleId = '\0' + virtualModuleId
+import { Plugin, ResolvedConfig } from 'vite'
+
+const virtualModuleId = 'virtual:my-module'
+const resolvedVirtualModuleId = '\0' + virtualModuleId
+const virtualEnvModuleId = 'virtual:env'
+const resolvedEnvVirtualModuleId = '\0' + virtualEnvModuleId
+
+function myPlugin(): Plugin {
+  let config: ResolvedConfig | null = null
 
   return {
-    name: 'my-plugin', // 必须的，将会在 warning 和 error 中显示
+    name: 'my-plugin',
+    configResolved(c: ResolvedConfig) {
+      config = c
+    },
     resolveId(id) {
       if (id === virtualModuleId) {
         return resolvedVirtualModuleId
+      }
+      if (id === virtualEnvModuleId) {
+        return resolvedEnvVirtualModuleId
       }
     },
     load(id) {
       if (id === resolvedVirtualModuleId) {
         return `export const msg = "from virtual module"`
       }
-    },
+      if (id === resolvedEnvVirtualModuleId) {
+        return `export default ${JSON.stringify(config!.env)}`
+      }
+    }
   }
 }
+export default myPlugin
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import inspect from 'vite-plugin-inspect';
 import virtual from './plugins/virtual-module'
+import svgr from './plugins/svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),virtual()],
+  plugins: [svgr({ defaultExport: 'url' }), inspect()]
 })
